6b202e0081bdd879378b018941a206b5b3cdd96a
Rokus Ottervanger <rokus@ruvu.nl>
2020-12-09 06:08:58 +0100

    Refactor/apply ros standards (#8)
    * fix: use sensor_msgs::Imu for IMU data
    
    * fix: put SI units (Tesla) in magnetic field msg
    
    * refactor: clean up unused publishers
    
    * refactor: clean up callbacks that dont do anything
    
    * refactor: push drive in namespace from launch file, remove ns from topic names
    
    * refactor: lookup parameters in nodes private namespace
    
    * refactor: use rosparam tooling for parameter defaults
    
    * fix: use tcp_ip_port variable, from param, to set server port
    
    * fix: add missing ros msg header information
    
    * refactor: remove useless and commented code
    
    * refactor: follow package naming conventions

--- src/duro.cpp
+++ src/duro.cpp
@@ -1,11 +1,7 @@
 // ros headers
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-#include "std_msgs/Float64.h"
-#include "std_msgs/Int16.h"
-#include "std_msgs/Int8.h"
 #include "std_msgs/UInt8.h"
-#include "std_msgs/String.h"
 #include "nav_msgs/Odometry.h"
 #include "sensor_msgs/NavSatFix.h"
 #include "sensor_msgs/NavSatStatus.h"
@@ -13,8 +9,7 @@
 #include "sensor_msgs/MagneticField.h"
 #include "geometry_msgs/Vector3.h"
 #include "geometry_msgs/PoseStamped.h"
-#include "tf/transform_broadcaster.h"
-#include <tf2_ros/static_transform_broadcaster.h>
+#include "tf2/LinearMath/Quaternion.h"
 // standard c headers
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -35,18 +30,18 @@
 sensor_msgs::NavSatFix fix;
 ros::Publisher nav_fix_pub;
 ros::Publisher odom_pub;
-ros::Publisher chatter_pub_st;
 ros::Publisher imu_pub;
 ros::Publisher mag_pub;
-ros::Publisher gyro_pub;
 ros::Publisher euler_pub;
 ros::Publisher pose_pub;
-ros::Publisher tmp_pub;
 ros::Publisher status_flag_pub;
 ros::Publisher status_stri_pub;
 
-std::string tcp_ip_addr = "";
-int tcp_ip_port = -1;
+std::string tcp_ip_addr;
+int tcp_ip_port;
+std::string gps_receiver_frame_id;
+std::string imu_frame_id;
+std::string utm_frame_id;
 static sbp_msg_callbacks_node_t heartbeat_callback_node;
 static sbp_msg_callbacks_node_t pos_ll_callback_node;
 static sbp_msg_callbacks_node_t orientation_callback_node;
@@ -56,9 +51,6 @@ static sbp_msg_callbacks_node_t imu_callback_node;
 static sbp_msg_callbacks_node_t mag_callback_node;
 nav_msgs::Odometry odom;
 geometry_msgs::PoseStamped pose_msg;
-// Debug broadcast tf
-// geometry_msgs::TransformStamped static_transformStamped;
-
 
 CoordinateTransition coordinate_transition;
 
@@ -76,7 +68,7 @@ void setup_socket()
   memset(&server, '0', sizeof(server));
   server.sin_addr.s_addr = inet_addr(tcp_ip_addr.c_str());
   server.sin_family = AF_INET;
-  server.sin_port = htons(55555);
+  server.sin_port = htons(tcp_ip_port);
 
   if (connect(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0)
   {
@@ -87,15 +79,6 @@ void setup_socket()
 void close_socket()
 {
   close(socket_desc);
-}
-
-void heartbeat_callback(u16 sender_id, u8 len, u8 msg[], void *context)
-{
-  (void)sender_id, (void)len, (void)msg, (void)context;
-  //fprintf(stdout, "%s\n", __FUNCTION__);
-  /*for(int i = 0; i < len; i++)
-    printf("%d ", msg[i]);
-  printf("len: %d\n", len);*/
 }
 
 // first three bits are fix mode
@@ -131,10 +114,10 @@ enum INS_MODE
 
 void pos_ll_callback(u16 sender_id, u8 len, u8 msg[], void *context)
 {
-  (void)sender_id, (void)len, (void)msg, (void)context;
   msg_pos_llh_t *latlonmsg = (msg_pos_llh_t *)msg;
   // nav fix (latlon) message over ROS
   fix.header.stamp = ros::Time::now();
+  fix.header.frame_id  = gps_receiver_frame_id;
 
   int ins_mode = (latlonmsg->flags & INS_MODE_MASK) >> INS_MODE_POSITION;  // INS mode seems to remain 0...
   int fix_mode = (latlonmsg->flags & FIX_MODE_MASK) >> FIX_MODE_POSITION;
@@ -161,22 +144,21 @@ void pos_ll_callback(u16 sender_id, u8 len, u8 msg[], void *context)
 
     double x = 0, y = 0;
     coordinate_transition.LatLonToUTMXY(latlonmsg->lat, latlonmsg->lon, x, y);
+
     odom.header.stamp = ros::Time::now();
+    odom.header.frame_id = utm_frame_id;
+    odom.child_frame_id = gps_receiver_frame_id;
     odom.pose.pose.position.x = x;
     odom.pose.pose.position.y = y;
     odom.pose.pose.position.z = latlonmsg->height;
     odom_pub.publish(odom);
+
     pose_msg.header.stamp = ros::Time::now();
+    pose_msg.header.frame_id = utm_frame_id;
     pose_msg.pose.position.x = x;
     pose_msg.pose.position.y = y;
     pose_msg.pose.position.z = 0; //latlonmsg->height;
-    // Debug broadcast tf
-    //static_transformStamped.header.stamp = ros::Time::now();
-    //static_transformStamped.header.frame_id = "/map";
-    //static_transformStamped.child_frame_id = "/duro";
-    //static_transformStamped.transform.translation.x = x;
-    //static_transformStamped.transform.translation.y = y;
-    //static_transformStamped.transform.translation.z = 0;
+
     pose_pub.publish(pose_msg); //
 
     switch (fix_mode)
@@ -214,7 +196,6 @@ void pos_ll_callback(u16 sender_id, u8 len, u8 msg[], void *context)
         ROS_WARN_STREAM("Acquired a fix with a mode that's not implemented. You are likely"
                         "using an unsupported version of libsbp.");
         stflags.data = "Not implemented";
-        break;
     }
 
     nav_fix_pub.publish(fix);
@@ -226,7 +207,6 @@ void orientation_callback(u16 sender_id, u8 len, u8 msg[], void *context)
 {
   // enable MSG ID 544 in swift console
   // the MSG ID comes from eg #define SBP_MSG_ORIENT_QUAT 0x0220 --> 544
-  (void)sender_id, (void)len, (void)msg, (void)context;
   msg_orient_quat_t *orimsg = (msg_orient_quat_t *)msg;
 
   double w = orimsg->w * pow(2, -31);
@@ -241,20 +221,11 @@ void orientation_callback(u16 sender_id, u8 len, u8 msg[], void *context)
   pose_msg.pose.orientation.x = tf_aligned.y();      // left-handerd / right handed orientation
   pose_msg.pose.orientation.y = tf_aligned.x() * -1; // left-handerd / right handed orientation
   pose_msg.pose.orientation.z = tf_aligned.z();      // left-handerd / right handed orientation
-  pose_msg.header.frame_id = "map";
-  // Debug broadcast tf
-  //static tf2_ros::StaticTransformBroadcaster static_broadcaster;
-  //static_transformStamped.transform.rotation.x = x;
-  //static_transformStamped.transform.rotation.y = y;
-  //static_transformStamped.transform.rotation.z = z;
-  //static_transformStamped.transform.rotation.w = w;
-  //static_broadcaster.sendTransform(static_transformStamped);
 }
 
 void orientation_euler_callback(u16 sender_id, u8 len, u8 msg[], void *context)
 {
   // enable MSG ID 545 in swift console
-  (void)sender_id, (void)len, (void)msg, (void)context;
   msg_orient_euler_t *orimsg = (msg_orient_euler_t *)msg;
   geometry_msgs::Vector3 eulervect;
   eulervect.x = orimsg->roll / 57292374.; // 57292374: raw > microdegrees > rad constant
@@ -263,94 +234,69 @@ void orientation_euler_callback(u16 sender_id, u8 len, u8 msg[], void *context)
   euler_pub.publish(eulervect);
 }
 
-void time_callback(u16 sender_id, u8 len, u8 msg[], void *context)
-{
-  (void)sender_id, (void)len, (void)msg, (void)context;
-  msg_gps_time_t *timemsg = (msg_gps_time_t *)msg;
-  //printf("%d\n", timemsg->tow);
-}
-
 void imu_callback(u16 sender_id, u8 len, u8 msg[], void *context)
 {
-  (void)sender_id, (void)len, (void)msg, (void)context;
   msg_imu_raw_t *imumsg = (msg_imu_raw_t *)msg;
-  geometry_msgs::Vector3 imu_ros_msg;
-  imu_ros_msg.x = imumsg->acc_x;
-  imu_ros_msg.y = imumsg->acc_y;
-  imu_ros_msg.z = imumsg->acc_z;
+  sensor_msgs::Imu imu_ros_msg;
+  imu_ros_msg.header.stamp = ros::Time::now();
+  imu_ros_msg.header.frame_id = imu_frame_id;
+  imu_ros_msg.linear_acceleration.x = imumsg->acc_x;
+  imu_ros_msg.linear_acceleration.y = imumsg->acc_y;
+  imu_ros_msg.linear_acceleration.z = imumsg->acc_z;
+
+  imu_ros_msg.angular_velocity.x = imumsg->gyr_x; // Angular rate around IMU frame X axis
+  imu_ros_msg.angular_velocity.y = imumsg->gyr_y;
+  imu_ros_msg.angular_velocity.z = imumsg->gyr_z;
   imu_pub.publish(imu_ros_msg);
-  geometry_msgs::Vector3 gyro_ros_msg;
-  gyro_ros_msg.x = imumsg->gyr_x; // Angular rate around IMU frame X axis
-  gyro_ros_msg.y = imumsg->gyr_y;
-  gyro_ros_msg.z = imumsg->gyr_z;
-  gyro_pub.publish(gyro_ros_msg);
 }
 
 void mag_callback(u16 sender_id, u8 len, u8 msg[], void *context)
 {
-  (void)sender_id, (void)len, (void)msg, (void)context;
   msg_mag_raw_t *magmsg = (msg_mag_raw_t *)msg;
-  sensor_msgs::MagneticField mag_ros_msg;       // is geometry_msgs::Vector3 better?
-  mag_ros_msg.magnetic_field.x = magmsg->mag_x; // Magnetic field in the body frame X axis [microteslas]
-  mag_ros_msg.magnetic_field.y = magmsg->mag_y;
-  mag_ros_msg.magnetic_field.z = magmsg->mag_z;
+  sensor_msgs::MagneticField mag_ros_msg;
+  mag_ros_msg.header.stamp = ros::Time::now();
+  mag_ros_msg.header.frame_id = imu_frame_id;
+
+  mag_ros_msg.magnetic_field.x = magmsg->mag_x * 1e-6; // Magnetic field in the body frame X axis [microteslas]
+  mag_ros_msg.magnetic_field.y = magmsg->mag_y * 1e-6;
+  mag_ros_msg.magnetic_field.z = magmsg->mag_z * 1e-6;
   mag_pub.publish(mag_ros_msg);
 }
 
 s32 socket_read(u8 *buff, u32 n, void *context)
 {
-  (void)context;
-  s32 result;
-
-  result = read(socket_desc, buff, n);
-  return result;
+  return read(socket_desc, buff, n);
 }
 
 int main(int argc, char **argv)
 {
-  int opt;
-  int result = 0;
   sbp_state_t s;
   ros::init(argc, argv, "duro");
   ros::NodeHandle n;
-  chatter_pub_st = n.advertise<std_msgs::String>("gps/duro/utmzone", 100);
-  odom_pub = n.advertise<nav_msgs::Odometry>("gps/duro/odom", 100);
-  pose_pub = n.advertise<geometry_msgs::PoseStamped>("gps/duro/current_pose", 100);
-  nav_fix_pub = n.advertise<sensor_msgs::NavSatFix>("gps/duro/fix", 100);
-  mag_pub = n.advertise<sensor_msgs::MagneticField>("gps/duro/mag", 100);
-  imu_pub = n.advertise<geometry_msgs::Vector3>("gps/duro/imu", 100);
-  gyro_pub = n.advertise<geometry_msgs::Vector3>("gps/duro/gyro", 100);
-  euler_pub = n.advertise<geometry_msgs::Vector3>("gps/duro/rollpitchyaw", 100);
-  status_flag_pub = n.advertise<std_msgs::UInt8>("gps/duro/status_flag", 100);
-  status_stri_pub = n.advertise<std_msgs::String>("gps/duro/status_string", 100);
-  tmp_pub = n.advertise<std_msgs::Float64>("gps/duro/tmp", 100);
+  odom_pub = n.advertise<nav_msgs::Odometry>("odom", 100);
+  pose_pub = n.advertise<geometry_msgs::PoseStamped>("current_pose", 100);
+  nav_fix_pub = n.advertise<sensor_msgs::NavSatFix>("fix", 100);
+  mag_pub = n.advertise<sensor_msgs::MagneticField>("mag", 100);
+  imu_pub = n.advertise<sensor_msgs::Imu>("imu", 100);
+  euler_pub = n.advertise<geometry_msgs::Vector3>("rollpitchyaw", 100);
+  status_flag_pub = n.advertise<std_msgs::UInt8>("status_flag", 100);
+  status_stri_pub = n.advertise<std_msgs::String>("status_string", 100);
 
-  n.getParam("duro_address", tcp_ip_addr);
-  n.getParam("duro_port", tcp_ip_port);
-  if (tcp_ip_addr.length() < 3)
-  {
-    ROS_WARN("No or wrong parameters provided, assuming _address:=192.168.1.10 _port:=55555");
-    tcp_ip_addr = "192.168.1.10";
-    tcp_ip_port = 55555;
-  }
-  else if (tcp_ip_port == -1)
-  {
-    tcp_ip_port = 55555;
-  }
-  else
-  {
-    ROS_INFO("Starting with _address:=%s _port:=%d", tcp_ip_addr.c_str(), tcp_ip_port);
-  }
+  ros::NodeHandle n_private("~");
+  n_private.param<std::string>("ip_address", tcp_ip_addr, "192.168.0.222");
+  n_private.param<int>("port", tcp_ip_port, 55555);
+  n_private.param<std::string>("gps_receiver_frame_id", gps_receiver_frame_id, "duro_link");
+  n_private.param<std::string>("imu_frame_id", imu_frame_id, gps_receiver_frame_id);
+  n_private.param<std::string>("utm_frame_id", utm_frame_id, "utm");
+  ROS_INFO("Connecting to duro on %s:%d", tcp_ip_addr.c_str(), tcp_ip_port);
+
   setup_socket();
   sbp_state_init(&s);
-  sbp_register_callback(&s, SBP_MSG_HEARTBEAT, &heartbeat_callback, NULL, &heartbeat_callback_node);
   sbp_register_callback(&s, SBP_MSG_POS_LLH, &pos_ll_callback, NULL, &pos_ll_callback_node);
   sbp_register_callback(&s, SBP_MSG_ORIENT_QUAT, &orientation_callback, NULL, &orientation_callback_node);
   sbp_register_callback(&s, SBP_MSG_ORIENT_EULER, &orientation_euler_callback, NULL, &orientation_euler_callback_node);
-  sbp_register_callback(&s, SBP_MSG_GPS_TIME, &time_callback, NULL, &time_callback_node);
   sbp_register_callback(&s, SBP_MSG_IMU_RAW, &imu_callback, NULL, &imu_callback_node);
   sbp_register_callback(&s, SBP_MSG_MAG_RAW, &mag_callback, NULL, &mag_callback_node);
-  //ros::Rate loop_rate(10);
 
   while (ros::ok())
   {
@@ -358,5 +304,5 @@ int main(int argc, char **argv)
     ros::spinOnce();
   }
   close_socket();
-  return result;
+  return 0;
 }
